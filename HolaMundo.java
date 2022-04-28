cdec0b1366b145fcea06a0530f5ee7bc13f10986
Anthony Joaquin Ticona Hareth <aticonaha@unsa.edu.pe>
2022-04-28 12:38:19 -0500

    Presentación al curso

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo{
 	public static void main(String[] args){
 	System.out.println("Hola mundo");
+	System.out.println("Este es el laboratorio de PW2, soy Anthony Joaquin Ticona Hareth, estudiante.");
 	}
 }
